10d4f9caf81dd191817d3ed9fd4ac5f2d321062d
Anton Vasilev <bysslaev@gmail.com>
2018-07-05 19:53:11 +0300

    Добавил новый сервис для поиска прокси.

---
+++ src/service/UxProxyOrgService.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anton Vasiliev <bysslaev@gmail.com>
+ * Date: 05/07/2018
+ * Time: 19:24
+ */
+
+namespace ExvmProxyParser\Service;
+
+/**
+ * Class UxProxyOrgService
+ * @package ExvmProxyParser\Service
+ */
+class UxProxyOrgService extends Service
+{
+    /**
+     * UxProxyOrgService constructor.
+     * @param $config
+     * @throws \Exception
+     */
+    public function __construct($config)
+    {
+        if (empty($config['url'])) {
+            throw new \Exception('Config property \'url\' must be configured.');
+        }
+        $this->url = $config['url'];
+        parent::__construct($config);
+    }
+
+    public function startParse($nextPage = null, $prepareDom = false)
+    {
+        parent::startParse($nextPage, $prepareDom);
+    }
+
+    protected function findProxiesInDom($dom)
+    {
+        $lines = explode("\n", $dom);
+
+        $list = [];
+
+        foreach ($lines as $line) {
+            list($ip, $port) = explode(":", $line);
+            $list[] = [
+                "ip" => $ip,
+                "port" => $port,
+                "protocol" => "http"
+            ];
+        }
+
+        return $list;
+    }
+
+}
